4833d07f2b75bfd59b74ffcc89b2fee8be922f65
Johan Angelstam <johan791@astmatix.ida.liu.se>
2010-02-16 12:57:58 +0100

    Created simple client and server.

--- src/client.adb
+++ src/client.adb
@@ -1,8 +1,48 @@
 with Ada.Text_IO;                        use Ada.Text_IO;
+with Ada.Command_Line;                   use Ada.Command_Line;
+with Ada.Exceptions;                     use Ada.Exceptions;
+with Ada.Integer_Text_IO;                use Ada.Integer_Text_IO;
+
+with TJa.Sockets;                        use TJa.Sockets;
 
 procedure Client is
+
+   Socket : Socket_Type;
+   Port : Natural;
+
+   Text      : String(1..100);
+   Text_Length : Natural;
+
 begin
-   Put("I haz clientz");
+
+   if Argument_Count /= 2 then
+      Raise_Exception(Constraint_Error'Identity,
+                      "Usage: " & Command_Name & " remotehost remoteport");
+   end if;
+
+   Port := Natural'Value(Argument(2));
+
+   Initiate(Socket);
+
+   Connect(Socket, Argument(1), Port);
+
+   loop
+
+      Put_Line("Skriv en sträng, skriv exit för att avsluta");
+      Get_Line(Text,Text_Length);
+      if Text_Length=100 then Skip_Line; end if;
+
+      exit when Text(1..4) = "exit";
+
+      Put_Line(Socket,Text(1..Text_Length));
+
+      Get_Line(Socket,Text,Text_Length);
+      Put_Line(Text(1..Text_Length));
+
+   end loop;
+
+   Close(Socket);
+
 end Client;
 
 
--- src/server.adb
+++ src/server.adb
@@ -1,8 +1,42 @@
 with Ada.Text_IO;                        use Ada.Text_IO;
+with Ada.Integer_Text_IO;                use Ada.Integer_Text_IO;
+with Ada.Command_Line;                   use Ada.Command_Line;
+with Ada.Exceptions;                     use Ada.Exceptions;
+
+with TJa.Sockets;                        use TJa.Sockets;
 
 procedure Server is
+   Listener : Listener_Type;
+
+   Socket : Socket_Type;
+
+   Port : Natural;
+
+   Text      : String(1..100); --Används för att ta emot text
+   Text_Length : Natural;        --Kommer innehålla längden på denna text
+
 begin
-   Put("I haz server");
+   if Argument_Count /= 1 then
+      Raise_Exception(Constraint_Error'Identity,
+                      "Usage: " & Command_Name & " port");
+   end if;
+
+   Port := Natural'Value(Argument(1));
+
+   Initiate(Listener,Port);
+   Put("Listening for connection...");
+   Wait_For_Connection(Listener,Socket);
+   Put(Ascii.Cr);
+   Put_Line("Client connected...");
+   loop
+      Get_Line(Socket,Text,Text_Length);
+      Put_Line("Client sent: " & Text(Text_Length));
+      Put_Line(Socket,"You sent: " & Text(Text_Length));
+
+   end loop;
+
+   Close(Socket);
+
 end Server;
 
 
