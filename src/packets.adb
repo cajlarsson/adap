d76827276a11395f73780aef825b4c6825730c8d
Johan Angelstam <johan791@zaza10.ida.liu.se>
2010-02-22 15:17:02 +0100

    Added methods for socket IO to packets

--- src/packets.adb
+++ src/packets.adb
@@ -1,23 +1,39 @@
-with Ada.Strings.Unbounded;              use Ada.Strings.Unbounded;
-with Tja.Calendar;                       use Tja.Calendar;
-
-
 package body Packets is
 
-   function Assemble_Packet(Head: Character; content : U_String) return U_String is
+   function Assemble_Packet(Head: Character; Content: U_String) return U_String is
    begin
-      return Time_Now & ' ' & Head & ' ' & content;
+      return Time_Now & ' ' & Head & ' ' & Content;
    end;
 
-   function Packet_Head(Packet : U_String) return Character is
+   function Assemble_Packet(Head: Character; Content: Integer) return U_String is
+   begin
+      return Time_Now & ' ' & Head & ' ' & To_Unbounded_String(Content);
+   end;
+
+   function Packet_Head(Packet: U_String) return Character is
    begin
       return To_String(Packet)(10);
    end Packet_Head;
 
-   function Packet_Content(Packet : U_String) return U_String is
+   function Packet_Content(Packet: U_String) return U_String is
    begin
       return To_Unbounded_String(Slice(Packet, 12, Length(Packet)));
    end Packet_Content;
+
+   procedure Get_Line (Socket: in out Socket_Type; Item: out U_String) is
+      Buffer: String(1..100);
+      Length: Positive := 100;
+   begin
+      while Length = 100 loop
+         Get_Line(Socket, Buffer, Length);
+         Item := Item & To_Unbounded_String(Buffer(1..Length));
+      end loop;
+   end Get_Line;
+
+   procedure Put_Line (Socket: in out Socket_Type; Item: in U_String) is
+   begin
+      Put_Line(Socket,To_String(Item));
+   end Put_Line;
 
    function Time_Now return U_String is
    begin
--- src/packets.ads
+++ src/packets.ads
@@ -1,5 +1,8 @@
 with Ada.Strings.Unbounded;              use Ada.Strings.Unbounded;
 
+with Tja.Calendar;                       use Tja.Calendar;
+with TJa.Sockets;                        use TJa.Sockets;
+
 package Packets is
    subtype U_String is Unbounded_String;
 
@@ -11,13 +14,17 @@ package Packets is
    RESULT_HEAD : constant Character := 'R';
    FINISH_HEAD : constant Character := 'Q';
 
-   function Assemble_Packet(Head: Character; content : U_String) return U_String;
-   function Packet_Head(Packet : U_String) return Character;
-   function Packet_Content(Packet : U_String) return U_String;
+   function Assemble_Packet(Head: Character; Content: U_String) return U_String;
+   function Assemble_Packet(Head: Character; Content: Integer) return U_String;
+   function Packet_Head(Packet: U_String) return Character;
+   function Packet_Content(Packet: U_String) return U_String;
+
+   procedure Get_Line (Socket: in out Socket_Type; Item: out U_String);
+   procedure Put_Line (Socket: in out Socket_Type; Item: in U_String);
+
+
 
 private
    function Time_Now return U_String;
 
 end Packets;
-
-
