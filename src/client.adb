2acaa5ae2ec4cc709cd74eb7bb590430e75d86d3
Erik Lindholm <erili322@zaza4.ida.liu.se>
2010-02-22 15:35:50 +0100

    Working client conforming to specs

--- src/client.adb
+++ src/client.adb
@@ -2,23 +2,53 @@ with Ada.Text_IO;                        use Ada.Text_IO;
 with Ada.Command_Line;                   use Ada.Command_Line;
 with Ada.Exceptions;                     use Ada.Exceptions;
 with Ada.Integer_Text_IO;                use Ada.Integer_Text_IO;
+with Ada.Strings.Unbounded;              use Ada.Strings.Unbounded;
 
 with TJa.Sockets;                        use TJa.Sockets;
+
+with Packets;                            use Packets;
 
 procedure Client is
 
    Socket : Socket_Type;
    Port : Natural;
+   UserID : String(1..32);
+   UserID_Length : Natural;
+   Password : String(1..32);
+   Password_Length : Natural;
+   Figure_Number : Natural;
+   Text : U_String;
+   Text_Length: Natural;
 
-   Text      : String(1..100);
-   Text_Length : Natural;
+   procedure Login is
+
+   begin
+      Put_Line("Please enter your prefered UserID and Password,");
+      Put_Line("Do however note that both are stored unencrypted and are thus not secure.");
+      Get_Line(UserID,UserID_Length);
+      Get_Line(Password,Password_Length);
+
+      Put_Line(Socket,UserID(1..UserID_Length));
+      Put_Line(Socket,Password(1..Password_Length));
+
+      Get_Line(Socket,Text);
+      if Text_Length /= 2 then
+         Raise_Exception(Constraint_Error'Identity,
+                         "Userdata not accepted by server");
+         Close(Socket);
+      end if;
+
+      Put_Line(Socket,Assemble_Packet(NICK_HEAD,To_Unbounded_String("Fistosaurus")));--  FixMes: Nick,
+   end Login;
 
 begin
 
    if Argument_Count /= 2 then
-      Raise_Exception(Constraint_Error'Identity,
+      Raise_Exception(Constraint_Error'Identity,--  FixMes: ett bättre Exception
                       "Usage: " & Command_Name & " remotehost remoteport");
    end if;
+
+----------------------------------------------------------------------------------------
 
    Port := Natural'Value(Argument(2));
 
@@ -26,21 +56,9 @@ begin
 
    Connect(Socket, Argument(1), Port);
 
-   loop
+   Login;
 
-      Put_Line("Skriv en sträng, skriv exit för att avsluta");
-      Get_Line(Text,Text_Length);
-      if Text_Length=100 then Skip_Line; end if;
-
-      exit when Text(1..4) = "exit";
-
-      Put_Line(Socket,Text(1..Text_Length));
-
-      Get_Line(Socket,Text,Text_Length);
-      Put_Line(Text(1..Text_Length));
-
-   end loop;
-
+   Put_Line(Socket,Assemble_Packet(FORFEIT_HEAD,Figure_Number));
    Close(Socket);
 
 end Client;
