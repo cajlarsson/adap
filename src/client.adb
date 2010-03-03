d854dbcedce039b94f6a30dda4a5aaf96ee7463b
Erik Lindholm <erili322@astmatix.ida.liu.se>
2010-03-03 21:23:50 +0100

    Release candidate for client. Everything but graphix.

--- src/client.adb
+++ src/client.adb
@@ -1,9 +1,11 @@
 with Ada.Text_IO;                        use Ada.Text_IO;
+--with Ada.Strings_Edit.Integer;           use Ada.Strings_Edit.Integer;
 with Ada.Command_Line;                   use Ada.Command_Line;
 with Ada.Exceptions;                     use Ada.Exceptions;
 with Ada.Strings;                        use Ada.Strings;
 with Ada.Integer_Text_IO;                use Ada.Integer_Text_IO;
 with Ada.Strings.Unbounded;              use Ada.Strings.Unbounded;
+with Ada.Characters;                     use Ada.Characters;
 
 with TJa.Sockets;                        use TJa.Sockets;
 
@@ -22,10 +24,11 @@ procedure Client is
    Password : String(1..32);
    Password_Length : Natural;
    Figure_Number : Natural;
-   Text : U_String;
+   Text,Input,T1,T2 : U_String;
    Text_Length: Natural;
    Delbeskrivning : FP_A;
-   Ind : Integer;
+   Ind,I2 : Integer;
+   Lösis : Full_Part;
 
 
 ----------------------------------------------------------------------------------------
@@ -35,9 +38,7 @@ procedure Client is
    begin
       Put_Line(Socket,Assemble_Packet(FORFEIT_HEAD,Figure_Number));
       Close(Socket);
-
    end Fail;
-
 ----------------------------------------------------------------------------------------
 
    procedure Login is
@@ -51,9 +52,8 @@ procedure Client is
       Put_Line(Socket,Password(1..Password_Length));
 
       Get_Line(Socket,Text);
-      if Text_Length /= 2 then
+      if Text /= "OK" then
          Put(To_String(Text));
-      else
          Raise_Exception(Constraint_Error'Identity,
                          "Userdata not accepted by server");
          Close(Socket);
@@ -62,6 +62,13 @@ procedure Client is
       Put_Line(Socket,Assemble_Packet(NICK_HEAD,To_Unbounded_String("Fistosaurus")));--  FixMes: Nick,
    end Login;
 ----------------------------------------------------------------------------------------
+
+   function Solver(Lösis : Full_Part; Delbeskrivning : FP_A)return FP_A is
+   begin
+     return null;
+      end Solver;
+----------------------------------------------------------------------------------------
+--procedure Haz_Draz
 
 begin
 
@@ -79,25 +86,54 @@ begin
    Login;
 
    While true loop                       --FixMe: Truue? ORLY?
+
+      Get_Line(Socket,Text);
+
       Case Packet_Head(Text) is
          when 'D' =>
-            Ind := Integer'Value(To_string(Head(Trim(Packet_Content(Text),Left),
-                                        Index(Trim(Packet_Content(Text),Left)," "))));
+            Input := Trim(Packet_Content(Text),Left);
+            Ind := Integer'Value(To_string(Head(Input,Index(Input," "))));
             Delbeskrivning := new Full_Part_Array(1..Ind);
+            Input := Delete(Input,1,Index(Input," "));
 
             for I in 1..Ind loop
-               null;
+               T2 := Tail(Input,Length(Input) - Index(Input," "));
+               I2 := Index(T2," ");
+               T1 := Head(Input,I2);
+               Delbeskrivning(I):= Get(T1,I);
+               Input :=Tail(T2,Length(T2) - Index(T2," "));
                end loop;
 
-            -- FixMe: Delbeskrivning
+            -- FixMe: Testing
          when 'F' =>
-            null;
-            -- FixMe: Figurbeskrivning
+            Input := Trim(Packet_Content(Text),Left);
+            Ind := Integer'Value(To_string(Head(Input,Index(Input," "))));
+            Lösis := Get(Delete(Input,1,Index(Input," ")),Ind);
+
+            Text := To_Unbounded_String(Integer'Image(Delbeskrivning'Last));
+            Text := Trim(Text,Left);
+            Delbeskrivning := Solver(Lösis,Delbeskrivning);
+            for I in Delbeskrivning'Range loop
+               Text := Text & " ";
+               Text :=Text & Put(Delbeskrivning(I));
+            end loop;
+            Put_Line(Socket,Assemble_Packet(ROTATION_HEAD,Text));
+              -- FixMe: Testing
          when 'R' =>
-            null;
-            -- FixMe: Resultat
+            Input := Packet_Content(Text);
+            Input := Head(Tail(Input,3),1);
+            case To_String(Input)(1) is
+               when 's' =>
+                 Put_Line("Qapla'!");
+               when 'r' =>
+                 Put_Line("We gave up and succeded at it!");
+               when others =>
+                 Put_Line("Garrdak");
+            end case;
+
+            -- FixMe: Better text
          when 'Q' =>
-            null;
+            Put(To_String(Packet_Content(Text)));
             -- Quit
          when others =>
            null;
